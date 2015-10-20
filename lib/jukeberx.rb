cba72e074ebad3aba0454cb84f54e855f45ddb23
Brian Dunlap <briandunlap589@gmail.com>
2015-10-20 11:45:59 -0400

    Add song and searcher files.

--- lib/jukeberx.rb
+++ lib/jukeberx.rb
@@ -1,4 +1,10 @@
 require "jukeberx/version"
+require "jukeberx/song"
+require "jukeberx/searcher"
+
+require "sinatra"
+require "mp3info"
+require "pry"
 
 module Jukeberx
   # Your code goes here...
